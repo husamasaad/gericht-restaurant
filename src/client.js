143acac7533dc67b0d19640408257ee155abdbaf
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2023-05-14 01:38:59 +0200

    changed useCdn to false

--- src/client.js
+++ src/client.js
@@ -1,11 +1,13 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url'
 
 
-export const client = sanityClient({
+export const client = createClient({
   projectId: '2uav2qwr',
   dataset: 'production',
   apiVersion: '2023-05-09',
+  useCdn: false,
+  token: process.env.SANITY_API_TOKEN
 })
 
 const builder = imageUrlBuilder(client)
