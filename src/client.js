cf52171ffa1f2d949673164ac84902750f246702
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2023-05-14 02:50:25 +0200

    removed .env file

--- src/client.js
+++ src/client.js
@@ -7,7 +7,6 @@ export const client = createClient({
   dataset: 'production',
   apiVersion: '2023-05-09',
   useCdn: false,
-  token: 'skFoFL8bHcgwcHHDFRh0rbsatIJMHuGP3XIWnWMECAZ2hwmHUugQrnwXsdUqkzCiJbNEfSc4nr6xfffaq5CrqHGg36LG0ibPquhivDw9ul4k1IyfotLje8rDhVAtCKw1xBm6sCMKMBZ7OhdHR1TwuQjyqomVNSbnavkKnkAQwzjuzAGBh2R3'
 })
 
 const builder = imageUrlBuilder(client)
